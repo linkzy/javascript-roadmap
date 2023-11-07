ae4f80572f844a24cb3b8e1d0d06106589027f75
Danilo Cavalcante <danilo.ut@gmail.com>
2023-11-07 16:25:17 -0300

    add example of data structures

---
+++ basic-concepts/dataStuctures.js
@@ -0,0 +1,33 @@
+//https://linkzy.dev/understanding-javascript-data-structures/
+
+// Number
+let age = 30;
+
+// String
+let name = "John";
+
+// Boolean
+let isStudent = true;
+
+// Null
+let emptyValue = null;
+
+// Undefined
+let uninitializedValue;
+
+// Object
+let person = { name: "Alice", age: 25 };
+
+// Array
+let numbers = [1, 2, 3, 4, 5];
+
+// Function
+function greet(name) {
+  console.log("Hello, " + name + "!");
+}
+
+// Symbol
+const uniqueKey = Symbol("unique");
+
+// BigInt
+let largeNumber = 1234567890123456789012345678901234567890n;
